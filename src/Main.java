6d03f4f86ae8a6379a6fb1dabe603d6234299cab
ioann <yova@mail.ru>
2022-08-01 21:58:59 +0300

    peremennie home

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        int i = 7;
+        short s = 100;
+        byte b = 5;
+        long a = 300L;
+        float f = 52.825f;
+        double d = 72.39;
+        boolean = true;
+        char c = 4;
+
+
+
         System.out.println("Hello world!");
     }
 }
